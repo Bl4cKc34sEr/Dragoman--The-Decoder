c24561600f0ac07a4263a7631bec8865b959fd13
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-04-24 09:42:52 +0530

    Update Hex.py

--- Hex.py
+++ Hex.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
+# By -- Bl4cKc34sEr
+# Might be the smallest code u would have seen 😂....
 
 a=input()
 bytearray.fromhex(a).decode()
+
+# OR you can try this too....
+bytearray.fromhex(input()).decode()
+
+
 
