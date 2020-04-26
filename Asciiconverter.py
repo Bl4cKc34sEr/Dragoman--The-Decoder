ba37ed3ea2ebe357996f6ac19fbfe5b42276d8bd
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-04-26 15:26:05 +0530

    Update Asciiconverter.py

--- Asciiconverter.py
+++ Asciiconverter.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
+import os
+import sys
+import re
+def main():
+    if os.name == 'nt':
+        os.system('cls')
+    banner = """\u001b[36m
+         ___                               _    __       
+        / _ \___ ________ ___ _  ___ ___  (_)__/ /__ ____
+       / ___/ _ `/ __/ _ `/  ' \(_-</ _ \/ / _  / -_) __/
+      /_/   \_,_/_/  \_,_/_/_/_/___/ .__/_/\_,_/\__/_/   
+                                  /_/     \u001b[0m               
+                            
+                           \u001b[32m - coded with <3 by Devansh Batham\u001b[0m 
+    """
+    print(banner)
 
 message = input("Enter ASCII codes: ") #Decoding ascii to text
 
