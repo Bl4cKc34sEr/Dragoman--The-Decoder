d13d522bee9fa0af41a01806069c720c2d985d7c
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-05-03 18:29:55 +0530

    Update Asciiconverter.py

--- Asciiconverter.py
+++ Asciiconverter.py
@@ -8,32 +8,37 @@ banner = """\u001b[36;1m
  ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░  ░   ░   ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░
  ░ ░  ░   ░░   ░   ░   ▒   ░ ░   ░ ░ ░ ░ ▒  ░      ░     ░   ▒      ░   ░ ░ 
    ░       ░            ░  ░      ░    ░ ░         ░         ░  ░         ░ 
-                \u001b[32;1m
-\u001b[35;1m                 ___   _ _                  __       
+                \u001b[32;1m                        
+                                             --Presented with <3 by Shivanshu Sharma
+\u001b[35           ___           _ _         _      __       
         / _ | ___ ____(_|_) / _ \___ _______  ___/ /__ ____
        / __ |(_-</ __/ / / / // / -_) __/ _ \/ _  / -_) __/
       /_/ |_/___/\__/_/_/ /____/\__/\__/\___/\_,_/\__/_/  
-
-                              \u001b[32;1m --Presented with <3 by Shivanshu Sharma\u001b[0m 
-    """
+\u001b[32;1m \u001b[0m    """
 print(banner) 
 menu=""" \u001b[33;1m
-----------------------------------------
-            OTHER DECODERS             |
-----------------------------------------
-  [+] CEASER DECODER     -- Ceaser.py  |
-  [+] BACON DECODER      -- Bacon.py   |
-  [+] BASE64 DECODER     -- Base64.py  |
-  [+] DNA DECODER        -- Dna.py     | 
-  [+] MORSE DECODER      -- Morse.py   | 
-  [+] NUMBER SYSTEM      -- Num.py     | 
-        [!] BINARY TO TEXT             | 
-        [!] HEX TO TEXT                | 
-        [!] OCTAL TO TEXT              |  
-  [+] RAILFENCE DECODER  -- Rail.py    |  
-  [+] ROT13 DECODER      -- Rot.py     | 
-  [+] VIGNERE DECODER    -- Vignere.py |
----------------------------------------
+------------------------------------------
+            ALL DECODERS                 |
+------------------------------------------
+  [+] ASCII CONVERTER      -- Ascii.py   |
+  [+] ATBASH DECODER       -- Atbash.py  |
+  [+] CEASER DECODER       -- Ceaser.py  |
+  [+] BACON DECODER        -- Bacon.py   |
+  [+] BASE32 DECODER       -- Base32.py  |
+  [+] BASE64 DECODER       -- Base64.py  |
+  [+] BASE85 DECODER       -- Base85.py  |
+  [+] DNA DECODER          -- Dna.py     | 
+  [+] MORSE DECODER        -- Morse.py   | 
+  [+] NUMBER SYSTEM        -- Num.py     | 
+        [!] BINARY TO TEXT               | 
+        [!] HEX TO TEXT                  | 
+        [!] OCTAL TO TEXT                |  
+  [+] RAILFENCE DECODER    -- Rail.py    |
+  [+] REVERSE CIPHER       -- Reverse.py |  
+  [+] ROT13 DECODER        -- Rot.py     | 
+  [+] TRANSPOSITION CIPHER -- Trans.py   | 
+  [+] VIGNERE DECODER      -- Vignere.py |
+------------------------------------------s
 \u001b[32;1m""" 
 print(menu)
 
@@ -49,4 +54,3 @@ message = input("Enter message to encode: ") #encoding text to ascii
 for ch in message:
     print ("Decoded string (in ASCII):")
     print (ord(ch))
-
