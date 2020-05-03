be933cb7706f535646a2fc2ce664d8a8cd9d84cd
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-05-03 19:33:27 +0530

    Update Base32.py

--- Base32.py
+++ Base32.py
@@ -1,16 +1,53 @@
-#!/usr/bin/env python
-# coding: utf-8
+banner = """\u001b[36;1m
+ █████▄  ██▀███   ▄▄▄       ▄████  ▒█████    ███▄ ▄███    ▄███    ▓▓▄     █ 
+▒██  ██▌▓██ ▒ ██▒▒████▄     ██▒ ▀█▒▒██   ██▒ ██▒▀█▀█ █ ▒██   ██ ▄ ██ ▀█   █ 
+░██   █▌▓██  ▄█ ▒▒██  ▀█▄  ▒██░▄▄▄░▒██░  ██ ▓██    ▓██▒ ██   ▀█▄▓ ██  ▀█ ██▒
+░▓█▄   ▌▒██▀▀█▄  ░██▄▄▄▄██ ░▓█  ██▓▒██   ██░▒██    ▒██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒
+░▒████▓ ░██▓ ▒██▒ ▓█   ▓██▒░▒▓███▀▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██░   ▓██░
+ ▒▒▓  ▒ ░ ▒▓ ░▒▓░ ▒▒   ▒█░ ░▒   ▒ ░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ 
+ ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░  ░   ░   ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░
+ ░ ░  ░   ░░   ░   ░   ▒   ░ ░   ░ ░ ░ ░ ▒  ░      ░     ░   ▒      ░   ░ ░ 
+   ░       ░            ░  ░      ░    ░ ░         ░         ░  ░         ░ 
+                \u001b[32;1m                        
+                                             --Presented with <3 by Shivanshu Sharma
 
-# In[8]:
+\u001b[35   ____                   _________  
+ | __ )  __ _ ___  ___  |___ /___ \ 
+ |  _ \ / _` / __|/ _ \   |_ \ __) |
+ | |_) | (_| \__ \  __/  ___) / __/ 
+ |____/ \__,_|___/\___| |____/_____|
+                                    
+\u001b[32;1m \u001b[0m    """
+print(banner) 
+menu=""" \u001b[33;1m
+------------------------------------------
+            LIST OF DECODERS             |
+------------------------------------------
+  [+] ASCII CONVERTER      -- Ascii.py   |
+  [+] ATBASH DECODER       -- Atbash.py  |
+  [+] CEASER DECODER       -- Ceaser.py  |
+  [+] BACON DECODER        -- Bacon.py   |
+  [+] BASE32 DECODER       -- Base32.py  |
+  [+] BASE64 DECODER       -- Base64.py  |
+  [+] BASE85 DECODER       -- Base85.py  |
+  [+] DNA DECODER          -- Dna.py     | 
+  [+] MORSE DECODER        -- Morse.py   | 
+  [+] NUMBER SYSTEM        -- Num.py     | 
+        [!] BINARY TO TEXT               | 
+        [!] HEX TO TEXT                  | 
+        [!] OCTAL TO TEXT                |  
+  [+] RAILFENCE DECODER    -- Rail.py    |
+  [+] REVERSE CIPHER       -- Reverse.py |  
+  [+] ROT13 DECODER        -- Rot.py     | 
+  [+] TRANSPOSITION CIPHER -- Trans.py   | 
+  [+] VIGNERE DECODER      -- Vignere.py |
+------------------------------------------s
+\u001b[32;1m""" 
+print(menu)
 
 
 import base64
 print("[!]Your Plain-Text will be dispalyed as b'Yourtext'")
 print (base64.b32decode(input("Enter Base32 String: ")))
-
-
-# In[ ]:
-
-
 
 
