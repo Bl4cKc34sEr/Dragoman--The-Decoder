0263929a426b074afc5807d3f8454def8505636e
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-05-04 17:16:54 +0530

    Update Vignere.py

--- Vignere.py
+++ Vignere.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 banner = """\u001b[36;1m
  █████▄  ██▀███   ▄▄▄       ▄████  ▒█████    ███▄ ▄███    ▄███    ▓▓▄     █ 
 ▒██  ██▌▓██ ▒ ██▒▒████▄     ██▒ ▀█▒▒██   ██▒ ██▒▀█▀█ █ ▒██   ██ ▄ ██ ▀█   █ 
@@ -12,12 +8,39 @@ banner = """\u001b[36;1m
  ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░  ░   ░   ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░
  ░ ░  ░   ░░   ░   ░   ▒   ░ ░   ░ ░ ░ ░ ▒  ░      ░     ░   ▒      ░   ░ ░ 
    ░       ░            ░  ░      ░    ░ ░         ░         ░  ░         ░ 
- ░                                                                         
-                           
-                           \u001b[32;1m - coded with <3 For CTF's by Shivanshu Sharma\u001b[0m 
-    """
-print(banner)  
+                \u001b[32;1m                        
+                                             --Presented with <3 by Shivanshu Sharma
 
+\u001b[35  _      _   __    _      ____  ___   ____      __    _   ___   _     ____  ___  
+\ \  / | | / /`_ | |\ | | |_  | |_) | |_      / /`  | | | |_) | |_| | |_  | |_) 
+ \_\/  |_| \_\_/ |_| \| |_|__ |_| \ |_|__     \_\_, |_| |_|   |_| | |_|__ |_| \ 
+\u001b[32;1m \u001b[0m    """
+print(banner) 
+menu=""" \u001b[33;1m
+------------------------------------------
+            LIST OF DECODERS             |
+------------------------------------------
+  [+] ASCII CONVERTER      -- Ascii.py   |
+  [+] ATBASH DECODER       -- Atbash.py  |
+  [+] CEASER DECODER       -- Ceaser.py  |
+  [+] BACON DECODER        -- Bacon.py   |
+  [+] BASE32 DECODER       -- Base32.py  |
+  [+] BASE64 DECODER       -- Base64.py  |
+  [+] BASE85 DECODER       -- Base85.py  |
+  [+] DNA DECODER          -- Dna.py     | 
+  [+] MORSE DECODER        -- Morse.py   | 
+  [+] NUMBER SYSTEM        -- Num.py     | 
+        [!] BINARY TO TEXT               | 
+        [!] HEX TO TEXT                  | 
+        [!] OCTAL TO TEXT                |  
+  [+] RAILFENCE DECODER    -- Rail.py    |
+  [+] REVERSE CIPHER       -- Reverse.py |  
+  [+] ROTn DECODER         -- Rot.py     | 
+  [+] TRANSPOSITION CIPHER -- Trans.py   | 
+  [+] VIGNERE DECODER      -- Vignere.py |
+------------------------------------------s
+\u001b[32;1m""" 
+print(menu)
 a = input('Enter cipher text: ')
 b = input('Enter key: ')
 a = a.lower()
@@ -35,4 +58,3 @@ for i in a:
     if j == len(b):
         j = 0
 print ('The plain text: '+ c)
-
