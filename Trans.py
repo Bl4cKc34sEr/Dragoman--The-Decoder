fb6d36e90d54657b2620d951c2cc1bf8325be5f0
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-05-04 17:12:11 +0530

    Update and rename Transposition.py to Trans.py

---
+++ Trans.py
@@ -0,0 +1,106 @@
+banner = """\u001b[36;1m
+ █████▄  ██▀███   ▄▄▄       ▄████  ▒█████    ███▄ ▄███    ▄███    ▓▓▄     █ 
+▒██  ██▌▓██ ▒ ██▒▒████▄     ██▒ ▀█▒▒██   ██▒ ██▒▀█▀█ █ ▒██   ██ ▄ ██ ▀█   █ 
+░██   █▌▓██  ▄█ ▒▒██  ▀█▄  ▒██░▄▄▄░▒██░  ██ ▓██    ▓██▒ ██   ▀█▄▓ ██  ▀█ ██▒
+░▓█▄   ▌▒██▀▀█▄  ░██▄▄▄▄██ ░▓█  ██▓▒██   ██░▒██    ▒██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒
+░▒████▓ ░██▓ ▒██▒ ▓█   ▓██▒░▒▓███▀▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██░   ▓██░
+ ▒▒▓  ▒ ░ ▒▓ ░▒▓░ ▒▒   ▒█░ ░▒   ▒ ░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ 
+ ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░  ░   ░   ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░
+ ░ ░  ░   ░░   ░   ░   ▒   ░ ░   ░ ░ ░ ░ ▒  ░      ░     ░   ▒      ░   ░ ░ 
+   ░       ░            ░  ░      ░    ░ ░         ░         ░  ░         ░ 
+                \u001b[32;1m                        
+                                             --Presented with <3 by Shivanshu Sharma
+
+\u001b[35 ___ _  _     __ _  _  ___________ _        _  __ __ _  _  __ _ 
+ | |_)|_||\|(_ |_)/ \(_  |  |  | / \|\|   | \|_ /  / \| \|_ |_)
+ | | \| || |__)|  \_/__)_|_ | _|_\_/| |   |_/|__\__\_/|_/|__|\\
+\u001b[32;1m \u001b[0m    """
+print(banner) 
+menu=""" \u001b[33;1m
+------------------------------------------
+            LIST OF DECODERS             |
+------------------------------------------
+  [+] ASCII CONVERTER      -- Ascii.py   |
+  [+] ATBASH DECODER       -- Atbash.py  |
+  [+] CEASER DECODER       -- Ceaser.py  |
+  [+] BACON DECODER        -- Bacon.py   |
+  [+] BASE32 DECODER       -- Base32.py  |
+  [+] BASE64 DECODER       -- Base64.py  |
+  [+] BASE85 DECODER       -- Base85.py  |
+  [+] DNA DECODER          -- Dna.py     | 
+  [+] MORSE DECODER        -- Morse.py   | 
+  [+] NUMBER SYSTEM        -- Num.py     | 
+        [!] BINARY TO TEXT               | 
+        [!] HEX TO TEXT                  | 
+        [!] OCTAL TO TEXT                |  
+  [+] RAILFENCE DECODER    -- Rail.py    |
+  [+] REVERSE CIPHER       -- Reverse.py |  
+  [+] ROT13 DECODER        -- Rot.py     | 
+  [+] TRANSPOSITION CIPHER -- Trans.py   | 
+  [+] VIGNERE DECODER      -- Vignere.py |
+------------------------------------------s
+\u001b[32;1m""" 
+print(menu)
+import math
+
+def decrypt(message, keyword):
+  matrix = new1(ordering(keyword), message)
+
+  plaintext = "";
+  for r in range(len(matrix)):
+    for c in range (len(matrix[r])):
+      plaintext += matrix[r][c]
+  return plaintext
+
+
+def new1(keySeq, message):
+  width = len(keySeq)
+  height = math.ceil(len(message) / width)
+  if height * width < len(message):
+    height += 1
+
+  matrix = new2(width, height, len(message))
+
+  pos = 0
+  for num in range(len(keySeq)):
+    column = keySeq.index(num+1)
+
+    r = 0
+    while (r < len(matrix)) and (len(matrix[r]) > column):
+      matrix[r][column] = message[pos]
+      r += 1
+      pos += 1
+
+  return matrix
+
+
+def new2(width, height, length):
+  matrix = []
+  totalAdded = 0
+  for r in range(height):
+    matrix.append([])
+    for c in range(width):
+      if totalAdded >= length:
+        return matrix
+      matrix[r].append('')
+      totalAdded += 1
+  return matrix
+
+
+def ordering(keyword):
+  sequence = []
+  for pos, ch in enumerate(keyword):
+    previousLetters = keyword[:pos]
+    newNumber = 1
+    for previousPos, previousCh in enumerate(previousLetters):
+      if previousCh > ch:
+        sequence[previousPos] += 1
+      else:
+        newNumber += 1
+    sequence.append(newNumber)
+  return sequence
+
+cipher = input("Enter Ciphertext :")
+key = input("Enter the Key :")
+dec = decrypt(cipher,key)
+print(dec)
