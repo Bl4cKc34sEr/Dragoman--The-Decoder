64986f82d94e42f1241278c80a771c9447566f1c
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-05-04 15:26:03 +0530

    Update and rename Dna_Decode.py to Dna.py

--- Dna.py
+++ Dna.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-banner = """\u001b[36;1m
+ banner = """\u001b[36;1m
  █████▄  ██▀███   ▄▄▄       ▄████  ▒█████    ███▄ ▄███    ▄███    ▓▓▄     █ 
 ▒██  ██▌▓██ ▒ ██▒▒████▄     ██▒ ▀█▒▒██   ██▒ ██▒▀█▀█ █ ▒██   ██ ▄ ██ ▀█   █ 
 ░██   █▌▓██  ▄█ ▒▒██  ▀█▄  ▒██░▄▄▄░▒██░  ██ ▓██    ▓██▒ ██   ▀█▄▓ ██  ▀█ ██▒
@@ -12,11 +8,41 @@ banner = """\u001b[36;1m
  ░ ▒  ▒   ░▒ ░ ▒░  ▒   ▒▒ ░  ░   ░   ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░
  ░ ░  ░   ░░   ░   ░   ▒   ░ ░   ░ ░ ░ ░ ▒  ░      ░     ░   ▒      ░   ░ ░ 
    ░       ░            ░  ░      ░    ░ ░         ░         ░  ░         ░ 
- ░                                                                         
-                           
-                           \u001b[32;1m - coded with <3 For CTF's by Shivanshu Sharma\u001b[0m 
-    """
-print(banner)  
+                \u001b[32;1m                        
+                                             --Presented with <3 by Shivanshu Sharma
+
+\u001b[35 █▀▄ █▄░█ ▄▀█   █▀▄ █▀▀ █▀▀ █▀█ █▀▄ █▀▀ █▀█
+█▄▀ █░▀█ █▀█   █▄▀ ██▄ █▄▄ █▄█ █▄▀ ██▄ █▀▄
+                                       
+\u001b[32;1m \u001b[0m    """
+print(banner) 
+menu=""" \u001b[33;1m
+------------------------------------------
+            LIST OF DECODERS             |
+------------------------------------------
+  [+] ASCII CONVERTER      -- Ascii.py   |
+  [+] ATBASH DECODER       -- Atbash.py  |
+  [+] CEASER DECODER       -- Ceaser.py  |
+  [+] BACON DECODER        -- Bacon.py   |
+  [+] BASE32 DECODER       -- Base32.py  |
+  [+] BASE64 DECODER       -- Base64.py  |
+  [+] BASE85 DECODER       -- Base85.py  |
+  [+] DNA DECODER          -- Dna.py     | 
+  [+] MORSE DECODER        -- Morse.py   | 
+  [+] NUMBER SYSTEM        -- Num.py     | 
+        [!] BINARY TO TEXT               | 
+        [!] HEX TO TEXT                  | 
+        [!] OCTAL TO TEXT                |  
+  [+] RAILFENCE DECODER    -- Rail.py    |
+  [+] REVERSE CIPHER       -- Reverse.py |  
+  [+] ROT13 DECODER        -- Rot.py     | 
+  [+] TRANSPOSITION CIPHER -- Trans.py   | 
+  [+] VIGNERE DECODER      -- Vignere.py |
+------------------------------------------s
+\u001b[32;1m""" 
+print(menu)
+
+
 mapping = {
         
 'AAA':'a',
@@ -84,15 +110,15 @@ mapping = {
 'TTG':' ',
 'TTT':'.'
 
-	
-	
+    
+    
 }
 
 
 
 
 
-string = input()
+string = input("Enter DNA string: ")
 def decode_dna( string ):
 
     pieces = []
